e8340752e2bb4f12d53ee96f96dfdc558418a13f
Alesya <rabushka2014@mail.ru>
2024-10-04 22:31:59 +0300

    latest version so far

--- main.py
+++ main.py
@@ -1,174 +1,113 @@
 import cv2
-import copy
-from rectangles import *
 import random
-
-
-
-# получение маски изображения
-def get_image_mask(img, threshold = -1):
-    # размытие изображения
-    img_blured = cv2.medianBlur(img, 3)
-    cv2.imwrite('process-img/blured.png', img_blured)
-    
-    # изображение в чб
-    img_gray = cv2.cvtColor(img_blured, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite('process-img/gray.png', img_gray)
-
-    # получение ?цветной части изображения?
-    adaptiveThreshold = threshold if threshold >= 0 else cv2.mean(img)[0]
-    color = cv2.cvtColor(img_blured, cv2.COLOR_BGR2HLS)
-    mask = cv2.inRange(color, (0, int(adaptiveThreshold / 6), 60), (180, adaptiveThreshold, 255))
-
-    # создание маски цветной части изображения
-    result_mask = cv2.bitwise_and(img_gray, img_gray, mask=mask)
-    cv2.imwrite('process-img/mask.png', result_mask)
-
-    return result_mask
-
-# полчуение контура с печатью на изображении
-def get_stamp_contour(img, mask):
-    # получение копии изображения для поиска контуров
-    img_copy = copy.deepcopy(img)
-
-    # поиск контуров на копии
-    cont, _ = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-    img_contoured = cv2.drawContours(img_copy, cont, -1, 255, 3)
-    cv2.imwrite('process-img/contoured.png', img_contoured)
-
-    c = max(cont, key = cv2.contourArea)
-    x,y,w,h = cv2.boundingRect(c)
-    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0),1)
-    
-
-    # new_contrs = copy.deepcopy(cont)
-    # for i in range(1, len(cont)):
-    #     x1,y1,w1,h1 = cv2.boundingRect(new_contrs[i - 1])
-    #     x2,y2,w2,h2 = cv2.boundingRect(new_contrs[i])
-
-    #     fact, cords = find_crossing_rectangles([x1,y1,w1,h1], [x2,y2,w2,h2])
-    #     cv2.rectangle(img, (cords[0],cords[1]), (cords[0] + cords[2], cords[1] + cords[3]), (125,255,0), 1)
-
-
-    # для поиска нескольких контуров
-    # new = []
-    # for c in cont:
-
-    #     area = cv2.contourArea(c)
-            
-    #     if area >= 1303:
-    #         # print('c', area)
-    #         print('---', area)
-    #         x,y,w,h = cv2.boundingRect(c)
-
-    #         # все вершины прямоугольника по часовой стрелке
-    #         # x y
-    #         # x y+h
-    #         # x+w y+h
-    #         # x+w y
-    #         new.append(c)
-
-    #         print(x,y,x+w,y+h)
-    #         # отрисовка найденного контура печати на оригинальном изображении
-    #         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 1)
-            
-
-
-    # for i in range(0, len(new)):
-    #     for j in range(0, len(new)):
-    #         if i != j:
-    #             result = contours_intersect(img, new[i], new[j])
-    #             print(result)
-    # #     x1,y1,w1,h1 = cv2.boundingRect(new[i - 1])
-    # #     x2,y2,w2,h2 = cv2.boundingRect(new[i])
-
-    # #     fact, cords = find_crossing_rectangles([x1,y1,w1,h1], [x2,y2,w2,h2])
-    # #     print(fact)
-    #             if result:
-    #                 coords1 = cv2.boundingRect(new[i])
-    #                 coords2 = cv2.boundingRect(new[j])
-                    
-    #                 x,y,w,h = get_rectangle_coords(coords1, coords2)
-
-    #                 cv2.rectangle(img, (x,y), (x+w, y+h), (125,240,0), 3)
-    #                 # cv2.imwrite('process-img/stamp_rect_img'+str(i) + str(j) +'.png', img)
-    
-
-
-    cv2.imwrite('process-img/stamp_rect_img.png', img)
-
-
-def stamp_recognition(img_path):
-    img = cv2.imread(img_path)
-
-    # получение маски изображения
-    mask = get_image_mask(img)
-
-    #
-    get_stamp_contour(img, mask)
 
 
 from ultralytics import YOLO
 
 
 
-def get_class(id):
-    if id == 0:
-        return 'dog'
+    
+class ObjectDetection:
+     
+    """
+    класс предназначен для распознавания наличия печати
+    на изображении документа
+    """
+    def __init__(self, weight_path, img_path):
+        # путь к изображению для распознания печатей на нем
+        self.img_path = img_path
+
+        # путь к весовым коэффициентам
+        self.weight_path = weight_path
+
+        # яркие цвета для отображения областей печатей
+        self.colors = [(255, 0, 255), (255, 255, 0), (0, 255, 0), (255, 165, 0),
+                        (0, 255, 255), (255, 0, 0), (128, 0, 128), (0, 0, 255)]
+
+        # модель для распознавания
+        self.model = YOLO(self.weight_path)
+
+        # количество печатей
+        self.stamps = 0
+
+
+     # получение названия класса, исходя из его идентификатора
+    def get_class(self, id):
+        if id == 0:
+            return 'stamp'
+
+
+    # метод распознавания печатей
+    def predict(self):
+        self.results = self.model.predict(source=self.img_path)
+          
+   
+
+    # отрисовка прямоугольников с печатями
+    # на изображении
+    def draw_rectangles(self):
+         # преобразование картинки для распознавания
+        img = cv2.imread(self.img_path)
+        print(self.results)
+
+        # проходим по результатам распознавания
+        for result in self.results:
+            # определяем все рамки, в которых находятся распознанные объекты
+            boxes = result.boxes.cpu().numpy()
+
+            # получаем количество обнаруженных печатей
+            self.stamps = len(boxes)
+
+            # список координат верхнего левого угла каждой рамки
+            coordinates_list = boxes.xyxy
+
+            # список вероятностей правильного распознавания сообтветствующих рамок
+            confidences_list = boxes.conf
+
+            # список идентификаторов классов
+            classes_list = boxes.cls
+        
+
+        for result_id in range(0, len(confidences_list)):
+
+                # получаем имя класса по его идентификатору
+                result_class_name = self.get_class(classes_list[result_id])
+
+                # получаем показатель степени уверенности в верном распознавании объекта
+                result_conf = str(round(confidences_list[result_id] * 100, 3))
+
+                # получаем координаты рамки
+                result_coords = coordinates_list[result_id]
+
+                x = int(result_coords[0])
+                y = int(result_coords[1])
+                w = int(result_coords[2])
+                h = int(result_coords[3])
+
+                # рандомно определяем цвет рамки
+                color = self.colors[random.randint(1,100) % len(self.colors)]
+
+                # отображаем рамку на изображении
+                cv2.rectangle(img, (x,y), (w,h), (color), 3)
+
+                # отображаем название класса и степень уверенности над рамкой объекта на итоговом изображении
+                cv2.putText(img, f'{result_class_name}, {result_conf}%', (x+5, y-10), cv2.FONT_HERSHEY_SIMPLEX, 2, color, 2, cv2.LINE_AA)
+                
+        # сохраняем изменения в итоговое изображение
+        cv2.imwrite('result-img/new_img.png', img)
+
+        if self.stamps == 0:
+             print("\nПечатей не обнаружено")
+        else:
+            print(f"\nОбнаружено печатей в количестве: {self.stamps} ")
+    
+         
 
 if __name__ == '__main__':
-    img_path = 'data/images/val/n02085620_1502.jpg'
+    weights =  'best.pt'
+    image_path = 'img/1.jpg'
 
-    model = YOLO('/home/aleksa/UNIVER/image-validation-stamp-recognition/runs/detect/train24/weights/best.pt')
-    
-    results = model.predict(source=img_path)
-
-    colors = [(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) for j in range(10)]
-
-    img = cv2.imread(img_path)
-
-    # проходим по результатам распознавания
-    for result in results:
-        # определяем все рамки, в которых находятся распознанные объекты
-        boxes = result.boxes.cpu().numpy()
-
-        # список координат верхнего левого угла каждой рамки
-        coordinates_list = boxes.xyxy
-
-        # список вероятностей правильного распознавания сообтветствующих рамок
-        confidences_list = boxes.conf
-
-        # список идентификаторов классов
-        classes_list = boxes.cls
-
-    for result_id in range(0, len(confidences_list)):
-
-        # отметаем все "объекты" с низким уроовнем уверенности
-        if confidences_list[result_id] >= 0.5:
-
-            # получаем имя класса по его идентификатору
-            result_class_name = get_class(classes_list[result_id])
-
-            # получаем показатель степени уверенности в верном распознавании объекта
-            result_conf = str(confidences_list[result_id])
-
-            # получаем координаты рамки
-            result_coords = coordinates_list[result_id]
-
-            x = int(result_coords[0])
-            y = int(result_coords[1])
-            w = int(result_coords[2])
-            h = int(result_coords[3])
-
-            # рандомно определяем цвет рамки
-            color = colors[random.randint(1,100) % len(colors)]
-
-            # отображаем рамку на изображении
-            cv2.rectangle(img, (x,y), (w,h), (color), 3)
-
-            # отображаем название класса и степень уверенности над рамкой объекта на итоговом изображении
-            cv2.putText(img, f'{result_class_name}, {result_conf}%', (x, y+15), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 1, cv2.LINE_AA)
-            
-    # сохраняем изменения в итоговое изображение
-    cv2.imwrite('new_img.png', img)
-    
+    detection = ObjectDetection(weights, image_path)
+    detection.predict()
+    detection.draw_rectangles()
+   
--- model/model.py
+++ model/model.py
@@ -1,43 +1,58 @@
 from ultralytics import YOLO
-import torch
-import cv2
+
+class Model():
+    """
+    класс предназначен для хранения готовой модели
+    и её обучения для определенного датасета
+    """
+    def __init__(self):
+        # переменная, содержащая модель
+        self.model = YOLO("yolo8s.pt")
+
+    def train(self, data='config.yaml', epochs=30):
+        # обучение модели
+        result = self.model.train(data, epochs)
+
+        return result
 
 
-model = YOLO("yolov8s.pt")
 
-result = model.train(data='config.yaml', epochs=30)
 
-class ObjectDetection():
-    def __init__(self, capture_index):
-        self.capture_index = capture_index
+# model = YOLO("yolov8s.pt")
 
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        print('Using device: ', self.device)
+# result = model.train(data='config.yaml', epochs=30)
 
-        self.model = self.load_model()
+# class ObjectDetection():
+#     def __init__(self, capture_index):
+#         self.capture_index = capture_index
 
-    def load_model(self):
-        model = YOLO('yolo8s.pt')
-        model.fuse()
+#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#         print('Using device: ', self.device)
 
-        return model
+#         self.model = self.load_model()
+
+#     def load_model(self):
+#         model = YOLO('yolo8s.pt')
+#         model.fuse()
+
+#         return model
     
-    def predict(self, img):
-        results = self.model(img)
+#     def predict(self, img):
+#         results = self.model(img)
 
-        return results
+#         return results
     
-    def plot_bboxes(self, results, img):
-        xyxys = []
-        confidences = []
-        class_ids = []
+#     def plot_bboxes(self, results, img):
+#         xyxys = []
+#         confidences = []
+#         class_ids = []
 
-        for result in results:
-            boxes = result.boxes.cpu().numpy()
+#         for result in results:
+#             boxes = result.boxes.cpu().numpy()
 
-            xyxys = boxes.xyxy
+#             xyxys = boxes.xyxy
 
-        for xyxy in xyxys:
-            cv2.rectangle(img, (int(xyxy[0], int(xyxy[1])), (int(xyxy[2], int(xyxy[3])), (0,255,0))))
+#         for xyxy in xyxys:
+#             cv2.rectangle(img, (int(xyxy[0], int(xyxy[1])), (int(xyxy[2], int(xyxy[3])), (0,255,0))))
         
-        cv2.imwrite('new_img.png', img)
+#         cv2.imwrite('new_img.png', img)
